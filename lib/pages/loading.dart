ad97fea0ead2237998cf6d191ac026c7d3b4c156
Mohammad Shohel <mohammadshohel866@gmail.com>
2021-09-29 18:34:23 +0600

    Api setup

---
+++ lib/pages/loading.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  String time = "loading";
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany', url: 'Europe/Berlin');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // getTime();
+    setupWorldTime();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      )
+    );
+  }
+}
+
